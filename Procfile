264c2789d36cf37fa9a0b6487a67d268483608b6
azhang4184 <andrewzhang4184@gmail.com>
2023-07-23 14:48:30 -0400

    reset

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python aramStatAnalyzer.py
+python aramStatAnalyzer.py
--- aramStatAnalyzer.py
+++ aramStatAnalyzer.py
@@ -1,21 +1,9 @@
 from flask import Flask, render_template, request
 import requests
-import os
+
 
 api_key = "RGAPI-f90e949d-d7d7-4a8e-81b1-108399607fa8"
-app = Flask(__name__, static_folder='static')
-port = int(os.environ.get("PORT", 5000))
-if __name__ == '__main__':
-    app.run(debug=False, port=port)
-
-def sort_an_array(input):
-    for i in range(len(input)):
-        for j in range(len(input) - 1):
-            if input[j][1] < input[j + 1][1]:
-                temp = input[j]
-                input[j] = input[j + 1]
-    return input
-
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -29,16 +17,19 @@ def index():
         print(api_url)
         resp = requests.get(api_url)
         player_info = resp.json()
-        print(player_info) 
+        print(player_info)
         player_puuid = player_info['puuid']
         pfp_id = player_info['profileIconId']
         summoner_lvl = player_info['summonerLevel']
+
 
         start = 0
         all_matches = []
         matches = []
 
+
         region = "americas"
+
 
         while True:
             api_url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{player_puuid}/ids?queue=450&start={start}"
@@ -52,11 +43,14 @@ def index():
             all_matches += matches
             print("hello")
 
+
         print(len(all_matches))
+
 
         win_data = {}
         kd_data = {}
         dmg_data = {}
+
 
         for match_id in all_matches:
             match_data = get_match_data(region, match_id)
@@ -73,17 +67,22 @@ def index():
                 kd_data[key] = [game_kd]
                 dmg_data[key] = [game_dmg]
 
+
         averages = {}
+
 
         for key in win_data:
             win_values = win_data[key]
             kd_values = kd_data[key]
             dmg_values = dmg_data[key]
 
+
             average_win = round(sum(win_values) / float(len(win_values)), 2)
             average_kd = round(sum(kd_values) / len(kd_values), 2)
             average_dmg = round(sum(dmg_values) / len(dmg_values), 2)
             num_games = len(win_values)
+
+
 
 
             averages[key] = {
@@ -93,9 +92,12 @@ def index():
                 "num_games": num_games
             }
 
+
         return render_template('stats.html', username=name, stats=averages,pfp = pfp_id,lvl = summoner_lvl)
 
+
     return render_template('index.html')
+
 
 def get_match_data(region, match_id):
     import time
@@ -109,13 +111,16 @@ def get_match_data(region, match_id):
         data = resp.json()
         return data
 
+
 def getChamp(puuid, match_data):
     player_index = match_data['metadata']['participants'].index(puuid)
     return match_data['info']['participants'][player_index]['championName']
 
+
 def win(puuid, match_data):
     player_index = match_data['metadata']['participants'].index(puuid)
     return int(match_data['info']['participants'][player_index]['win'])
+
 
 def kd(puuid, match_data):
     player_index = match_data['metadata']['participants'].index(puuid)
@@ -123,9 +128,13 @@ def kd(puuid, match_data):
     deaths = match_data['info']['participants'][player_index]['deaths']
     return kills / deaths if deaths != 0 else kills
 
+
 def dmg(puuid, match_data):
     player_index = match_data['metadata']['participants'].index(puuid)
     return match_data['info']['participants'][player_index]['totalDamageDealtToChampions']
 
+
 if __name__ == '__main__':
     app.run()
+
+
